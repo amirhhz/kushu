e787cfb9380ebb950e232b1e47175c235464ea91
Amir Hajizamani <Amir.Hajizamani@caplin.com>
2011-10-19 12:59:50 +0000

    Stubs for getting a deck from the app, logging in and out. Restored orange-y look for now.

--- app.js
+++ app.js
@@ -23,10 +23,17 @@ app.configure(function(){
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  app.use(express.logger("dev"));
 });
 
 app.configure('production', function(){
   app.use(express.errorHandler()); 
+});
+
+app.dynamicHelpers({
+	session: function (req, res) {
+		return req.session;
+	}
 });
 
 /*****************************************************************************/
@@ -41,6 +48,43 @@ app.get('/', function(req, res){
 app.get('/hello/:user', function(req, res){
 	res.write("Hello, " + req.params.user);
 	res.end();
+});
+
+app.get("/login", function (req, res) {
+	if (req.session.username) {
+		res.redirect("home");
+		res.end();
+	}
+	res.render("login", {title: "Log in to Kushu"});
+});
+
+app.post("/login", function (req, res) {
+	var username = req.body.username;
+	req.session.username = username;
+	res.redirect("/login");
+	res.end();
+});
+
+app.get("/logout", function (req, res) {
+	req.session.username = null;
+	res.redirect("/");
+	res.end();
+});
+
+app.get("/deck/:deckId", function (req, res) {
+	var deck = [
+		{q: "Capital of France", a: "Paris"},
+		{q: "Capital of Germany", a: "Berlin"}
+	];
+	if (req.xhr) {
+		res.send(deck);
+	}
+	res.render('deck', {title: "DECK No. " + req.params.deckId, deck: deck});
+});
+
+
+app.get("/deck/:deckId/:cardId", function (req, res) {
+
 });
 
 /*****************************************************************************/
