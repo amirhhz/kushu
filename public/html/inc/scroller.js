3f0696a72740a6aa88d5309a75f3ef8a870a0542
James Seymour <james.seymour@caplin.com>
2011-10-20 15:08:00 +0000

    Finalised the moving between cards and the actions relating to the buttons

--- public/html/inc/scroller.js
+++ public/html/inc/scroller.js
@@ -1,39 +1,95 @@
 var windowHeight = document.height;
 var windowWidth = document.width;
 
-
-var numberOfCardsInDeck = 4;
+var numberOfCardsInDeck = 6;
 var currentPositionInDeck = 0;
 
-if(windowWidth > 666){
+var WIDTH = 640;
+var HEIGHT = 386;
 
+var selectedButton;
+
+if(windowWidth < 320){
+	WIDTH = 240;
+	HEIGHT = 144;
+}else if(windowWidth < 480){
+	WIDTH = 320;
+	HEIGHT = 192;
+}else if(windowWidth < 640){
+	WIDTH = 480;
+	HEIGHT = 288;
+}else if(windowWidth < 800){
+	WIDTH = 640;
+	HEIGHT = 386;	
+}else{
+	WIDTH = 800;
+	HEIGHT = 480;	
 }
-
 
 $(function(){
       $("#myScroll").mbScrollable({
 		dir:"vertical",
-        width:666,
+        width:WIDTH,
         elementsInPage:1,
         elementMargin:0,
-        height:406,
+        height:HEIGHT,
         controls:"#controls",
         slideTimer:600,
         autoscroll:false,
         scrollTimer:2000 
       });
     });
-    
 
-function getCorrectButton () {
-	currentPositionInDeck++;
+function showHideDifficultyButtons() {
+	currentPositionInDeck++;	
+	resetColorOfButtons();
+	
+	var difficultyButtons = document.getElementById("difficultyButtons");	
+	
 	if(currentPositionInDeck < numberOfCardsInDeck - 1){
 		if(currentPositionInDeck % 2 != 0){
-			document.getElementsByClassName("next")[0].innerHTML = "Get Next Question!";
+			document.getElementById("nextCardButton").style.display = "none";
+			document.getElementsByClassName("next")[0].innerHTML = "<div align=\"center\">Next Question!</div>";
+			difficultyButtons.style.display = "block";
 		}else{
-			document.getElementsByClassName("next")[0].innerHTML = "Get Answer!";
+			document.getElementsByClassName("next")[0].innerHTML = "<div align=\"center\">Show Answer!</div>";
+			difficultyButtons.style.display = "none";
 		}
 	}else{
-		document.getElementsByClassName("next")[0].innerHTML = "End of Deck!";
+		document.getElementById("controls").innerHTML = "<div align=\"center\"><p><b>End Of Deck!</b></p></div>";
+		difficultyButtons.style.display = "block";
 	}
 }
+
+function updateCorrectButton(buttonId) {
+	
+	document.getElementById("nextCardButton").style.display = "block";
+	
+	resetColorOfButtons();
+	
+	if(buttonId == 0){
+		document.getElementsByClassName("positive")[0].style.backgroundColor = "#529214";
+		document.getElementsByClassName("positive")[0].style.color = "#FFF";
+		selectedButton = 0;
+	}else if(buttonId == 1){
+		document.getElementsByClassName("almost")[0].style.backgroundColor = "#6299c5";
+		document.getElementsByClassName("almost")[0].style.color = "#FFF";
+		selectedButton = 1;
+	}else{
+		document.getElementsByClassName("negative")[0].style.backgroundColor = "#d12f19";
+		document.getElementsByClassName("negative")[0].style.color = "#FFF";
+		selectedButton = 2;
+	}
+}
+
+function resetColorOfButtons(){
+	document.getElementsByClassName("positive")[0].style.backgroundColor = "";
+	document.getElementsByClassName("positive")[0].style.color = "";
+	document.getElementsByClassName("almost")[0].style.backgroundColor = "";
+	document.getElementsByClassName("almost")[0].style.color = "";
+	document.getElementsByClassName("negative")[0].style.backgroundColor = "";
+	document.getElementsByClassName("negative")[0].style.color = "";
+}
+
+
+
