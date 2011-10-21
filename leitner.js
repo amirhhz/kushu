d60c0750346a69e097d97d383906758d9fcc534c
Bodrul Alam <Bodrul.Alam@caplin.com>
2011-10-21 14:43:08 +0000

    leitner now handles updating the deck state object, corresponding tests added in leitnertests

--- leitner.js
+++ leitner.js
@@ -26,21 +26,55 @@ var deckState = {
 
 exports.getCurrentCard = function(stateOfDeck){
 	
-	
-	
 	if(stateOfDeck.state.revision.rev_finished){
 		return null;
-	}else{
+	} else {
 	return stateOfDeck.state.groups[
-	stateOfDeck.state.next_due.group][stateOfDeck.state.next_due.card];
+		stateOfDeck.state.next_due.group][stateOfDeck.state.next_due.card];
 	}
 }
 
-exports.setAnswerOfCurrentCard = function(stateOfDeck, answer){
+exports.answerCurrentCard = function(stateOfDeck, answer){
+
 	stateOfDeck.answerQueue.push(answer);
+	moveToNextCard(stateOfDeck);
 }
 
-exports.moveToNextCard = function(stateOfDeck){
+exports.updateState = function(stateOfDeck){
+	
+	var answers = stateOfDeck.answerQueue;
+	var currentGroups = stateOfDeck.state.groups;
+	var updatedGroups = [];
+	
+	for(var i=0; i<currentGroups.length; i++){
+		updatedGroups.push(new Array());
+	}
+	
+	for(var key in currentGroups) {
+		var group = currentGroups[key];
+		for(var index in group){
+			updatedGroups[answers.shift()].push(group[index]);
+		}
+	}
+	
+	var updatedDeckState = {
+		state: {
+			revision: {
+				rev_no: (stateOfDeck.state.revision.rev_no+1),
+				rev_finished: false
+			},
+			next_due: {
+				group: 0, card: 0
+			},
+			groups: updatedGroups,		
+		},
+		answerQueue: []
+	};
+	
+	return updatedDeckState; 
+}
+
+moveToNextCard = function(stateOfDeck){
 	
 	if(!stateOfDeck.state.revision.rev_finished){
 		if(stateOfDeck.state.groups[stateOfDeck.state.next_due.group].length-1 == stateOfDeck.state.next_due.card) {
@@ -54,19 +88,4 @@ exports.moveToNextCard = function(stateOfDeck){
 	if (stateOfDeck.state.next_due.group == stateOfDeck.state.groups.length){
 		stateOfDeck.state.revision.rev_finished = true;
 	}
-	
-}
-
-exports.updateCardWithAnswer = function(stateOfDeck){
-	
-}
-
-exports.updateString = function(stateOfDeck){
-	
-}
-
-function getGroupAndPosition(stateOfDeck){
-	
-	// the new state
-	return ""; 
 }
--- test/leitnerTests.js
+++ test/leitnerTests.js
@@ -8,8 +8,7 @@ module.exports = testCase({
     	this.deckState = {
 			state: {
 				revision: {
-					rev_no: 0,
-					rev_date: "",
+					rev_no: 1,
 					rev_finished: false
 				},
 				next_due: {
@@ -23,6 +22,24 @@ module.exports = testCase({
 			},
 			answerQueue: []
 		};
+		this.expectedUpdatedDeck = {
+			state: {
+				revision: {
+					rev_no: 2,
+					rev_finished: false
+				},
+				next_due: {
+					group: 0, card: 0
+				},
+				groups: [
+					[1,8],
+					[2,3,5,6,7,9],
+					[4,10]
+				],		
+			},
+			answerQueue: []
+		};
+		this.testAnswers = [0,1,1,2,1,1,1,0,1,2];
         callback();
     },
     
@@ -36,31 +53,43 @@ module.exports = testCase({
     },
     
     testSettingTheAnswerOfCardAddsToTheAnswerQueue: function(test){
-    	leitner.setAnswerOfCurrentCard(this.deckState, 1);
+    	leitner.answerCurrentCard(this.deckState, 1);
     	var expected = [1];
     	test.deepEqual(expected , this.deckState.answerQueue);
     	test.done();
     },
     
-    testAferMovingToTheNextCardTheCurrentCardIsSecondFromFirstGroup: function(test) {
-    	leitner.moveToNextCard(this.deckState);
+    testAferAnsweringCurrentCardTheCurrentCardIsSecondFromFirstGroup: function(test){
+    	leitner.answerCurrentCard(this.deckState, 0);
     	test.equal(2, leitner.getCurrentCard(this.deckState));
     	test.done();
     }, 
     
-    testWhenAtTheEndOfTheFirstGroupGettingNextCardMovesToFirstCardOfSecondGroup: function(test) {   	
+    testWhenAtTheEndOfTheFirstGroupAnsweringCurrentCardMovesToFirstCardOfSecondGroup: function(test) {   	
     	this.deckState.state.next_due.card = 3;
-    	leitner.moveToNextCard(this.deckState); 
+    	leitner.answerCurrentCard(this.deckState, 0); 
     	test.equal(5, leitner.getCurrentCard(this.deckState));
     	test.done();
     },
     
-    testWhenAtTheEndDeckTheNextCardIsNull: function(test) {
+    testWhenAnsweringTheLastCardInDeckTheNextCardIsNull: function(test) {
     	this.deckState.state.next_due.group = 2;
     	this.deckState.state.next_due.card = 1;
-    	leitner.moveToNextCard(this.deckState);
+    	leitner.answerCurrentCard(this.deckState, 0);
     	test.equal(null, leitner.getCurrentCard(this.deckState));
     	test.done();
+    },
+    
+    testWhenUpdatingStateCardsAreCorrectlyRearranged: function(test) {
+   
+   		for(var index in this.testAnswers){
+   			leitner.getCurrentCard(this.deckState);
+   			leitner.answerCurrentCard(this.deckState, this.testAnswers[index]);
+   		}
+
+    	var updatedDeck = leitner.updateState(this.deckState);
+    	test.deepEqual(this.expectedUpdatedDeck, updatedDeck);
+    	test.done();	
     }
 
     
