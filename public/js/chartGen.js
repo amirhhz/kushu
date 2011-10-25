f9a63162d15d2093b7d66547dbb7bd9166f38c24
Daniel Owen <daniel.owen@caplin.com>
2011-10-25 10:26:40 +0000

    show hide div functionality added

--- public/js/chartGen.js
+++ public/js/chartGen.js
@@ -1,5 +1,13 @@
+function showDiv(divID){
+	document.getElementById('chartDiv1').style.display = 'none';
+	document.getElementById('chartDiv2').style.display = 'none';
+	document.getElementById('chartDiv3').style.display = 'none';
+	document.getElementById(divID).style.display = 'block'; 	
+}
+
 function generateStreamChart2()
         {
+			showDiv('chartDiv1');
 
             var chart1 = new RGraph.Bar('chart1', [[12,15,19], [11,14, 9], [10,15,3]]);
             //chart1.Set('chart.units.pre', '$');
@@ -51,39 +59,41 @@ function generateStreamChart2()
         }
 		
 		function generatePie(){
-		     var chart1 = new RGraph.Pie('chart1', [41,37,16,6]); // Create the pie object
-            chart1.Set('chart.labels', ['StaffPics (41%)', 'Capitals (37%)', 'French (16%)', 'Other (6%)']);
-            chart1.Set('chart.labels.sticks', true);
-            chart1.Set('chart.gutter.left', 30);
-            chart1.Set('chart.gutter.right', 30);
-            chart1.Set('chart.gutter.top', 35);
-            chart1.Set('chart.gutter.bottom', 35);
-            chart1.Set('chart.title', "Work share");
-            chart1.Set('chart.title.vpos', 0.3);
-            chart1.Set('chart.shadow', false);
-            chart1.Set('chart.tooltips.effect', 'fade');
-            chart1.Set('chart.tooltips.event', 'onmousemove');
-            //chart1.Set('chart.exploded', [0,0,15,25]);
-            chart1.Set('chart.tooltips', [
+			showDiv('chartDiv2');
+			
+		     var chart2 = new RGraph.Pie('chart2', [41,37,16,6]); // Create the pie object
+            chart2.Set('chart.labels', ['StaffPics (41%)', 'Capitals (37%)', 'French (16%)', 'Other (6%)']);
+            chart2.Set('chart.labels.sticks', true);
+            chart2.Set('chart.gutter.left', 30);
+            chart2.Set('chart.gutter.right', 30);
+            chart2.Set('chart.gutter.top', 35);
+            chart2.Set('chart.gutter.bottom', 35);
+            chart2.Set('chart.title', "Work share");
+            chart2.Set('chart.title.vpos', 0.3);
+            chart2.Set('chart.shadow', false);
+            chart2.Set('chart.tooltips.effect', 'fade');
+            chart2.Set('chart.tooltips.event', 'onmousemove');
+            //chart2.Set('chart.exploded', [0,0,15,25]);
+            chart2.Set('chart.tooltips', [
                                         'Staff Photos (41%)',
                                         'Capital Cities (37%)',
                                         'French Nouns (16%)',
                                         'Other (6%)'
                                        ]
                                       );
-            chart1.Set('chart.highlight.style', '2d');
-            chart1.Set('chart.linewidth', 2);
-            chart1.Set('chart.strokestyle', 'rgba(255,255,255,1)');
-            chart1.Set('chart.shadow', true);
-            chart1.Set('chart.shadow.offsetx', 0);
-            chart1.Set('chart.shadow.offsety', 0);
-            chart1.Set('chart.shadow.blur', 15);
-            chart1.Set('chart.exploded', [0,0,20,20]);
+            chart2.Set('chart.highlight.style', '2d');
+            chart2.Set('chart.linewidth', 2);
+            chart2.Set('chart.strokestyle', 'rgba(255,255,255,1)');
+            chart2.Set('chart.shadow', true);
+            chart2.Set('chart.shadow.offsetx', 0);
+            chart2.Set('chart.shadow.offsety', 0);
+            chart2.Set('chart.shadow.blur', 15);
+            chart2.Set('chart.exploded', [0,0,20,20]);
             
             if (RGraph.isIE8()) {
-                chart1.Draw();
+                chart2.Draw();
             } else {
-                RGraph.Effects.Pie.Grow(chart1);
+                RGraph.Effects.Pie.Grow(chart2);
             }
 		
 		
@@ -91,35 +101,36 @@ function generateStreamChart2()
 		
         function generateStreamChart()
         {
-
-            var chart1 = new RGraph.Bar('chart1', [[12,15,19], [11,14, 9], [10,15,3], [3,1,2], [12,9,20], [15,2,8], [19,6,2]]);
-            //chart1.Set('chart.units.pre', '$');
-            chart1.Set('chart.title', '1Wk Progress (cards viewed)');
-            chart1.Set('chart.title.vpos', 0.5);
-            chart1.Set('chart.colors', ['red', 'blue', 'green']);
-            chart1.Set('chart.gutter.left', 40);
-            chart1.Set('chart.gutter.right', 5);
-            chart1.Set('chart.gutter.top', 40);
-            chart1.Set('chart.gutter.bottom', 50);
-            chart1.Set('chart.shadow', true);
-            chart1.Set('chart.shadow.color', '#aaa');
-            chart1.Set('chart.background.barcolor1', 'white');
-            chart1.Set('chart.background.barcolor2', 'white');
-            chart1.Set('chart.background.grid.hsize', 5);
-            chart1.Set('chart.background.grid.vsize', 5);
-            chart1.Set('chart.grouping', 'stacked');
-            chart1.Set('chart.labels', ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']);
-            chart1.Set('chart.labels.above', true);
-            chart1.Set('chart.key', ['Needs Work', 'Unsure', 'Correct']);
-            chart1.Set('chart.key.background', 'rgba(255,255,255,0.7)');
-            chart1.Set('chart.key.position', 'gutter');
-            chart1.Set('chart.key.position.gutter.boxed', false);
-            chart1.Set('chart.key.position.y', chart1.Get('chart.gutter.top') - 15);
-            chart1.Set('chart.key.border', false);
-            chart1.Set('chart.background.grid.width', 0.3); // Decimals are permitted
-            chart1.Set('chart.text.angle', 35);
-            chart1.Set('chart.strokestyle', 'rgba(0,0,0,0)');
-            chart1.Set('chart.tooltips.event', 'onmousemove');
+        	showDiv('chartDiv3');
+        	
+            var chart3 = new RGraph.Bar('chart3', [[12,15,19], [11,14, 9], [10,15,3], [3,1,2], [12,9,20], [15,2,8], [19,6,2]]);
+            //chart3.Set('chart.units.pre', '$');
+            chart3.Set('chart.title', '1Wk Progress (cards viewed)');
+            chart3.Set('chart.title.vpos', 0.5);
+            chart3.Set('chart.colors', ['red', 'blue', 'green']);
+            chart3.Set('chart.gutter.left', 40);
+            chart3.Set('chart.gutter.right', 5);
+            chart3.Set('chart.gutter.top', 40);
+            chart3.Set('chart.gutter.bottom', 50);
+            chart3.Set('chart.shadow', true);
+            chart3.Set('chart.shadow.color', '#aaa');
+            chart3.Set('chart.background.barcolor1', 'white');
+            chart3.Set('chart.background.barcolor2', 'white');
+            chart3.Set('chart.background.grid.hsize', 5);
+            chart3.Set('chart.background.grid.vsize', 5);
+            chart3.Set('chart.grouping', 'stacked');
+            chart3.Set('chart.labels', ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']);
+            chart3.Set('chart.labels.above', true);
+            chart3.Set('chart.key', ['Needs Work', 'Unsure', 'Correct']);
+            chart3.Set('chart.key.background', 'rgba(255,255,255,0.7)');
+            chart3.Set('chart.key.position', 'gutter');
+            chart3.Set('chart.key.position.gutter.boxed', false);
+            chart3.Set('chart.key.position.y', chart3.Get('chart.gutter.top') - 15);
+            chart3.Set('chart.key.border', false);
+            chart3.Set('chart.background.grid.width', 0.3); // Decimals are permitted
+            chart3.Set('chart.text.angle', 35);
+            chart3.Set('chart.strokestyle', 'rgba(0,0,0,0)');
+            chart3.Set('chart.tooltips.event', 'onmousemove');
             
             if (!RGraph.isIE8()) {
                 tooltipFunc = function (i)
@@ -131,12 +142,12 @@ function generateStreamChart2()
                     else if (r == 1) return 'Unsure';
                     else if (r == 0) return 'Correct';
                 }
-                chart1.Set('chart.tooltips', tooltipFunc);
+                chart3.Set('chart.tooltips', tooltipFunc);
             }
 
             if (!RGraph.isIE8()) {
-                RGraph.Effects.Bar.Grow(chart1);
+                RGraph.Effects.Bar.Grow(chart3);
             } else {
-                chart1.Draw();
+                chart3.Draw();
             }
         }
